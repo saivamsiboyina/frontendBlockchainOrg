41f81186e556c257c7cff25b3d812b8a9b24ee2e
anjanapriya01 <anjana.swathi@swymcorp.com>
2023-05-02 10:53:48 +0530

    Added 3 functions

--- src/components/BaseOrg.js
+++ src/components/BaseOrg.js
@@ -10,25 +10,117 @@ import {
 }
     from "@shopify/polaris";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { ethers } from "ethers";
+import YourContract from "../contractData.json";
 
 function BaseOrg() {
-    const handleDonor=()=>{
+    const [provider, setProvider] = useState(undefined);
+    const [signer, setSigner] = useState(undefined);
+    const [contract, setContract] = useState(undefined);
+    const [account, setAccount] = useState("");
+    const [userAddress, setUserAddress] = useState('');
+    const [baseAuthOrganization, setBaseAuthOrganization] = useState("");
+    useEffect(() => {
+        const init = async () => {
+            try {
+                // Get provider and signer
+                if (window.ethereum) {
+                    await window.ethereum.request({ method: "eth_requestAccounts" });
+                }
+                const provider = new ethers.providers.Web3Provider(window.ethereum);
+                setProvider(provider);
+                const signer = provider.getSigner();
+                setSigner(signer);
+
+                // console.log(YourContract.address);
+                const contract = new ethers.Contract(
+                    YourContract.address,
+                    YourContract.abi,
+                    signer
+                );
+                setContract(contract);
+
+                // Get the current account from Metamask wallet
+                const currentAccount = await signer.getAddress();
+                setAccount(currentAccount);
+                
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        init();
+    }, []);
+
+    const handleAssignPatientOrg = async () => {
+        try {
+            const data = contract.interface.encodeFunctionData("assignPatientOrg", [
+                userAddress,
+            ]);
+            // console.log(contract.methods.)
+            // const tx = {
+            //   to: YourContract.address,
+            //   data: data,
+            // };
+            const tx = await signer.sendTransaction({
+                to: YourContract.address,
+                data: data,
+            });
+            const result = await tx.wait();
+            console.log(result);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    const handleGetPatientOrg = async () => {
+            try {
+              const data = contract.interface.encodeFunctionData("getPatientOrgList");
+              console.log("Data", data);
+              //   console.log(contract.methods.getPatientOrgList());
+              //   const tx = {
+              //     to: YourContract.address,
+              //     data: data,
+              //   };
+              const tx = await signer.sendTransaction({
+                to: YourContract.address,
+                data: data,
+              });
+              // console.log("Data", data);
+              const receipt = await tx.wait();
+              // console.log("REciepit", receipt.result);
+              // const result = contract.interface.decodeFunctionResult(
+              //   "getPatientOrgList",
+              //   receipt.result
+              // );
+              // console.log(result);
+              const patientOrgList = await contract.getPatientOrgList();
+              console.log(patientOrgList);
+              // console.log("Result", result[0]);
         
+              // console.log("Trai", console.interface.decodeFunctionResult());
+            } catch (error) {
+              console.error(error);
+            }
+          };
+    const getBaseAuth= async ()=>{
+        const baseAuthAddress = await contract.baseAuthOrganization();
+        setBaseAuthOrganization(baseAuthAddress);
     }
-    const [value,setValue]=useState(0)
+    const [value, setValue] = useState(0)
     return (
-        <div style={{padding:"30px"}}>
+        <div style={{ padding: "30px" }}>
             <Stack vertical>
-            <DisplayText size="small">
-            Base Auth Address
-                </DisplayText> 
+                <DisplayText size="small">
+                    Base Auth Address
+                </DisplayText>
                 <Button
-                onClick={()=>setValue(8)}>
+                    onClick={getBaseAuth}>
                     Base Auth Address
                 </Button>
-                <hr/>
+                <p>{baseAuthOrganization}</p>
+                <hr />
                 <DisplayText size="small">
-                Assign Donor Org
+                    Assign Donor Org
                 </DisplayText>
                 <Stack vertical>
                     <TextField
@@ -38,13 +130,13 @@ function BaseOrg() {
                         autoComplete="off"
                     />
                     <Button
-                    onClick={handleDonor}>
+                        onClick={""}>
                         Submit
                     </Button>
                 </Stack>
-                <hr/>
+                <hr />
                 <DisplayText size="small">
-                Assign Patient Org
+                    Assign Patient Org
                 </DisplayText>
                 <Stack vertical spacing="tight">
                     <TextField
@@ -53,16 +145,18 @@ function BaseOrg() {
                         // onChange={setBloodGroup}
                         autoComplete="off"
                     />
-                    <Button>
+                    <Button
+                    onClick={handleAssignPatientOrg}
+                    >
                         Submit
                     </Button>
                 </Stack>
-                <hr/>
+                <hr />
                 <DisplayText size="small">
                     Get All Donors
                 </DisplayText>
                 <Stack vertical spacing="tight">
-                <TextField
+                    <TextField
                         label="Index"
                         // value={bloodGrou}
                         // onChange={setBloodGroup}
@@ -71,10 +165,10 @@ function BaseOrg() {
                     <Button>
                         Submit
                     </Button>
-                    </Stack>
-                    <hr/>
-                    <DisplayText size="small">
-                Get All Patients
+                </Stack>
+                <hr />
+                <DisplayText size="small">
+                    Get All Patients
                 </DisplayText>
                 <Stack vertical spacing="tight">
                     <TextField
@@ -87,17 +181,17 @@ function BaseOrg() {
                         Submit
                     </Button>
                 </Stack>
-                <hr/>  
+                <hr />
                 <DisplayText size="small">
-                Organ Matching
-                </DisplayText> 
+                    Organ Matching
+                </DisplayText>
                 <Button
-                onClick={()=>setValue(4)}>
-                   Organ Matching
+                    onClick={() => setValue(4)}>
+                    Organ Matching
                 </Button>
-                <hr/>
+                <hr />
                 <DisplayText size="small">
-               Success Map
+                    Success Map
                 </DisplayText>
                 <Stack vertical spacing="tight">
                     <TextField
@@ -110,26 +204,26 @@ function BaseOrg() {
                         Submit
                     </Button>
                 </Stack>
-                <hr/>
+                <hr />
                 <DisplayText size="small">
-                Get Donor Org List
-                </DisplayText> 
+                    Get Donor Org List
+                </DisplayText>
                 <Button
-                onClick={()=>setValue(6)}>
+                    onClick={() => setValue(6)}>
                     Get Donor Org List
                 </Button>
-                <hr/>
+                <hr />
                 <DisplayText size="small">
-                Get Patient Org List
-                </DisplayText> 
+                    Get Patient Org List
+                </DisplayText>
                 <Button
-                onClick={()=>setValue(7)}>
-                   Get Patient Org List
+                    onClick={handleGetPatientOrg}>
+                    Get Patient Org List
                 </Button>
             </Stack>
         </div>
-        
-        
+
+
     );
 }
 
